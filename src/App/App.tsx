bb7fb5a22020222a6aab075efa847d2c03c8dd55
nhat2012782 <gonbbb1@gmail.com>
2026-01-04 11:28:05 +0700

    Add error boundary

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ErrorInfo } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import publicRoutes from '~/routes';
 
@@ -13,6 +13,7 @@ import { RecoilRoot } from 'recoil';
 import { CssBaseline, Stack } from '@mui/material';
 import './App.css';
 import ToastContext from '~/contexts/ToastContext';
+import { ErrorBoundary } from 'react-error-boundary';
 
 // Create a client
 const queryClient = new QueryClient({
@@ -27,19 +28,36 @@ function App() {
   const router = createBrowserRouter(publicRoutes);
 
   return (
-    <Stack sx={{ width: 1, height: '100vh' }}>
-      <RecoilRoot>
-        <ThemeCustomization>
-          <CssBaseline />
-          <ToastContext>
-            <QueryClientProvider client={queryClient}>
-              <RouterProvider router={router} />
-              <ReactQueryDevtools initialIsOpen={false} />
-            </QueryClientProvider>
-          </ToastContext>
-        </ThemeCustomization>
-      </RecoilRoot>
-    </Stack>
+    <ErrorBoundary
+      fallbackRender={({ error, resetErrorBoundary }) => (
+        <div role="alert">
+          <p>Something went wrong:</p>
+          <pre style={{ color: 'red' }}>{error.message}</pre>
+          <button onClick={resetErrorBoundary}>Retry</button>
+        </div>
+      )}
+      onReset={(details) => {
+        // Reset the state of your app so the error doesn't happen again
+      }}
+      onError={(error: Error, info: ErrorInfo) => {
+        console.log('🚀 ~ App ~ info:', info);
+        console.log('🚀 ~ App ~ error:', error);
+      }}
+    >
+      <Stack sx={{ width: 1, height: '100vh' }}>
+        <RecoilRoot>
+          <ThemeCustomization>
+            <CssBaseline />
+            <ToastContext>
+              <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+                <ReactQueryDevtools initialIsOpen={false} />
+              </QueryClientProvider>
+            </ToastContext>
+          </ThemeCustomization>
+        </RecoilRoot>
+      </Stack>
+    </ErrorBoundary>
   );
 }
 
