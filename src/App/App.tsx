01f90c2c5583246e6b16f39500c321fade31f9f6
nhat2012782 <gonbbb1@gmail.com>
2026-01-04 12:05:46 +0700

    Remove babel

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { ErrorInfo } from 'react';
+import React, { ErrorInfo, Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import publicRoutes from '~/routes';
 
@@ -14,6 +14,7 @@ import { CssBaseline, Stack } from '@mui/material';
 import './App.css';
 import ToastContext from '~/contexts/ToastContext';
 import { ErrorBoundary } from 'react-error-boundary';
+import LoadingCircular from '~/components/LoadingCircular';
 
 // Create a client
 const queryClient = new QueryClient({
@@ -28,36 +29,38 @@ function App() {
   const router = createBrowserRouter(publicRoutes);
 
   return (
-    <ErrorBoundary
-      fallbackRender={({ error, resetErrorBoundary }) => (
-        <div role="alert">
-          <p>Something went wrong:</p>
-          <pre style={{ color: 'red' }}>{error.message}</pre>
-          <button onClick={resetErrorBoundary}>Retry</button>
-        </div>
-      )}
-      onReset={(details) => {
-        // Reset the state of your app so the error doesn't happen again
-      }}
-      onError={(error: Error, info: ErrorInfo) => {
-        console.log('🚀 ~ App ~ info:', info);
-        console.log('🚀 ~ App ~ error:', error);
-      }}
-    >
-      <Stack sx={{ width: 1, height: '100vh' }}>
-        <RecoilRoot>
-          <ThemeCustomization>
-            <CssBaseline />
-            <ToastContext>
-              <QueryClientProvider client={queryClient}>
-                <RouterProvider router={router} />
-                <ReactQueryDevtools initialIsOpen={false} />
-              </QueryClientProvider>
-            </ToastContext>
-          </ThemeCustomization>
-        </RecoilRoot>
-      </Stack>
-    </ErrorBoundary>
+    <Suspense fallback={<LoadingCircular fullHeight />}>
+      <ErrorBoundary
+        fallbackRender={({ error, resetErrorBoundary }) => (
+          <div role="alert">
+            <p>Something went wrong:</p>
+            <pre style={{ color: 'red' }}>{error.message}</pre>
+            <button onClick={resetErrorBoundary}>Retry</button>
+          </div>
+        )}
+        onReset={(details) => {
+          // Reset the state of your app so the error doesn't happen again
+        }}
+        onError={(error: Error, info: ErrorInfo) => {
+          console.log('🚀 ~ App ~ info:', info);
+          console.log('🚀 ~ App ~ error:', error);
+        }}
+      >
+        <Stack sx={{ width: 1, height: '100vh' }}>
+          <RecoilRoot>
+            <ThemeCustomization>
+              <CssBaseline />
+              <ToastContext>
+                <QueryClientProvider client={queryClient}>
+                  <RouterProvider router={router} />
+                  <ReactQueryDevtools initialIsOpen={false} />
+                </QueryClientProvider>
+              </ToastContext>
+            </ThemeCustomization>
+          </RecoilRoot>
+        </Stack>
+      </ErrorBoundary>
+    </Suspense>
   );
 }
 
